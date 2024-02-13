f94577112f7bcf90af8e63f32202b37eea5316e2
Aknniyet <157214579+Aknniyet@users.noreply.github.com>
2024-02-13 13:45:51 +0600

    Add files via upload

---
+++ feb02/src/Main.java
@@ -0,0 +1,60 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        try {
+
+            Class.forName("org.postgresql.Driver");
+            String url = "jdbc:postgresql://localhost:5432/postgres";
+
+
+            Properties authorization = new Properties();
+            authorization.put("user", "postgres");
+            authorization.put("password", "1224"); 
+
+
+            Connection connection = DriverManager.getConnection(url, authorization);
+
+            String sql = "INSERT INTO orders (customer_name, customer_surname, email, name, price)" +
+                    " VALUES ('John', 'Andersen', 'john0101@example.com', 'AirPods', 2106000)";
+            Statement statement2 = connection.createStatement();
+            statement2.executeUpdate(sql);
+
+
+            Statement statement = connection.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                    ResultSet.CONCUR_UPDATABLE);
+
+
+            ResultSet table = statement.executeQuery("SELECT * FROM orders");
+
+            table.first();
+            for (int j = 1; j <= table.getMetaData().getColumnCount(); j++) {
+                System.out.print(table.getMetaData().getColumnName(j) + "\t\t");
+            }
+            System.out.println();
+
+            table.beforeFirst();
+            while (table.next()) {
+                for (int j = 1; j <= table.getMetaData().getColumnCount(); j++) {
+                    System.out.print(table.getString(j) + "\t\t");
+                }
+
+                System.out.println();
+            }
+
+            if (table != null) { table.close(); }
+            if (statement != null) { statement.close(); }
+            if (connection != null) { connection.close(); }
+        } catch (Exception e) {
+            System.err.println("Error accessing database!");
+            e.printStackTrace();
+        }
+    }
+
+}
